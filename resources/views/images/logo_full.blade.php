ab444e6f3a89b134eed20e1c023177401ecd589c
Chris Colleter <chris.colleter@gmail.com>
2018-06-29 18:33:38 +0100

    Change readme from Laravel default to Chessageme. Add a public image folder and logo

---
+++ resources/views/images/logo_full.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chessageme Logo</title>
+</head>
+<body>
+	<img src="{{ URL::to('/') }}/images/chessageme_logo.png" alt="Chessageme Logo"/>
+</body>
+</html>
