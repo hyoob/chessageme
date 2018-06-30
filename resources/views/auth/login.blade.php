cd0cd1787c547c53496d1cd65a7ba719dc3fb44d
Chris Colleter <chris.colleter@gmail.com>
2018-06-30 04:01:30 +0100

    Add logo to login page. Dummy change to test prod release workflow.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
 
-
+            <p align="center"><img src="/images/chessageme_logo.png"></p>
 
             <div class="panel panel-default">
 
