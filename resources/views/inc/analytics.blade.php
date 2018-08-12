685771ab64bac5bc0793f987aba617b693d2a6fe
Chris Colleter <chris.colleter@gmail.com>
2018-08-12 15:46:06 +0100

    Correct Google Analytics script (put Adsense in previous commit).

--- resources/views/inc/analytics.blade.php
+++ resources/views/inc/analytics.blade.php
@@ -1,7 +1,9 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-5346442512726097",
-        enable_page_level_ads: true
-      });
-    </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121556371-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121556371-1');
+</script>
